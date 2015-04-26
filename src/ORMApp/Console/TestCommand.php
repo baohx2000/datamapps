205f21a416f3216ce53d52172153b6f3f0c55f21
Gordon <baohx2000@gmail.com>
2015-04-25 18:11:46 -0700

    break up tests

--- src/ORMApp/Console/TestCommand.php
+++ src/ORMApp/Console/TestCommand.php
@@ -15,25 +15,124 @@ use Doctrine\ORM\Query;
 use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use DoctrineProxy\__CG__\ORMApp\Entities\Person;
 use Entities\Address;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use ORMApp\Entities;
 
 class TestCommand extends Command {
+    const DO_CREATE = 'create';
+    const DO_READ = 'read';
+    const DO_UPDATE = 'update';
+    const DO_DELETE = 'delete';
+
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $em;
+
     protected function configure()
     {
         parent::configure();
 
-        $this->setName('test:ormapp');
+        $this->setName('test:ormapp')
+            ->addArgument('do', InputArgument::OPTIONAL, 'Which option? [create, read, update, delete]', 'create');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         /** @var ManagerRegistryHelper $emHelper */
         $emHelper = $this->getHelper('doctrine');
-        /** @var EntityManagerInterface $em */
-        $em = $emHelper->getManager('default');
+        $this->em = $emHelper->getManager('default');
 
+        switch ($input->getArgument('do')) {
+            case self::DO_CREATE:
+                $this->doCreate($input, $output);
+                break;
+            case self::DO_READ:
+                $this->doRead($input, $output);
+                break;
+            case self::DO_UPDATE:
+                $this->doUpdate($input, $output);
+                break;
+            case self::DO_DELETE:
+                $this->doDelete($input, $output);
+                break;
+            default:
+                $output->writeln('<error>Unknown do argument: '.$input->getArgument('do').'</error>');
+                break;
+        }
+
+    }
+
+    protected function doUpdate(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Updating person name & address line 1');
+
+        $q = $this->em->createQuery('SELECT p FROM ORMApp\Entities\Person p INNER JOIN p.addresses a INNER JOIN p.phones ph');
+        $results = $q->setMaxResults(1000)->getResult();
+
+        $namePrefix = sha1(microtime());
+
+        $start = microtime(true);
+        /** @var Entities\Person $p */
+        foreach ($results as $p) {
+            $p->setFirstName($namePrefix.$p->getId());
+            /** @var Entities\Address $a */
+            foreach ($p->getAddresses() as $a) {
+                $a->setLine1($namePrefix.$a->getId());
+            }
+        }
+        $this->em->flush();
+
+        $end = microtime(true);
+        $diff = $end-$start;
+        $output->writeln('Took '.$diff.'s');
+    }
+
+    protected function doDelete(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Removing them');
+        $start = microtime(true);
+
+        $people = $this->em->getRepository(Entities\Person::class)
+            ->findAll();
+        foreach ($people as $p) {
+            $this->em->remove($p);
+        }
+        $this->em->flush();
+
+        $end = microtime(true);
+        $diff = $end-$start;
+        $output->writeln('Took '.$diff.'s');
+    }
+
+    protected function doRead(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Loading them back up');
+        $memStart = memory_get_usage(true);
+        $start = microtime(true);
+        $people = $this->em->getRepository(Entities\Person::class)
+            ->findAll();
+        $end = microtime(true);
+        $diff = $end-$start;
+        $memEnd = memory_get_usage(true);
+
+        $output->writeln('Loaded '.count($people).' records');
+        $output->writeln('Used '.$memEnd - $memStart.' bytes');
+        $output->writeln('Took '.$diff.'s');
+
+        $output->writeln('Loading via DQL: SELECT p FROM ORMApp\Entities\Person p INNER JOIN p.addresses a INNER JOIN p.phones ph');
+        $start = microtime(true);
+        $q = $this->em->createQuery('SELECT p FROM ORMApp\Entities\Person p INNER JOIN p.addresses a INNER JOIN p.phones ph');
+        $results = $q->setMaxResults(1000)->getResult();
+        $end = microtime(true);
+        $diff = $end-$start;
+        $output->writeln('Took '.$diff.'s');
+    }
+
+    protected function doCreate(InputInterface $input, OutputInterface $output)
+    {
         $output->writeln('Inserting 5000 records (1000 persons, each with 2 phones and 2 addresses');
 
         $start = microtime(true);
@@ -73,51 +172,12 @@ class TestCommand extends Command {
                         ->setState('AZ')
                         ->setPostalCode('85281')
                 );
-            $em->persist($p);
+            $this->em->persist($p);
         }
-        $em->flush();
+        $this->em->flush();
+
         $end = microtime(true);
         $diff = $end-$start;
         $output->writeln('Took '.$diff.'s');
-
-        $em->clear(Entities\Person::class);
-        $em->clear(Entities\Address::class);
-        $em->clear(Entities\Phone::class);
-
-        $output->writeln('Loading them back up');
-        $memStart = memory_get_usage();
-        $start = microtime(true);
-        $people = $em->getRepository(Entities\Person::class)
-            ->findAll();
-        $end = microtime(true);
-        $diff = $end-$start;
-        $memEnd = memory_get_usage();
-        $output->writeln('Loaded '.count($people).' records');
-        $output->writeln('Used '.$memEnd - $memStart.' bytes');
-        $output->writeln('Took '.$diff.'s');
-
-        $em->clear(Entities\Person::class);
-        $em->clear(Entities\Address::class);
-        $em->clear(Entities\Phone::class);
-
-        $output->writeln('Loading via DQL: SELECT p FROM ORMApp\Entities\Person p INNER JOIN p.addresses a INNER JOIN p.phones ph');
-        $start = microtime(true);
-        $q = $em->createQuery("SELECT p FROM ORMApp\Entities\Person p INNER JOIN p.addresses a INNER JOIN p.phones ph");
-        $results = $q->setMaxResults(1000)->getResult();
-        $end = microtime(true);
-        $diff = $end-$start;
-        $output->writeln('Took '.$diff.'s');
-
-//        $output->writeln('Removing them');
-//        $start = microtime(true);
-//        foreach($people as $person) {
-//            $em->remove($person);
-//        }
-//        $em->flush();
-//        $end = microtime(true);
-//        $diff = $end-$start;
-//        $output->writeln('Took '.$diff.'s');
-
-
     }
 }
